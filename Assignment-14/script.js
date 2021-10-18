31337e50eefb7e3bb90a2701e60af4316c627150
dtr-beast <55450440+dtr-beast@users.noreply.github.com>
2021-10-18 07:17:26 +0530

    Updated Assignment-14

--- Assignment-14/script.js
+++ Assignment-14/script.js
@@ -1,49 +1,44 @@
 let cnt = 0;
-window.onload=function() {
-    console.log('Hi ');
-    setTimeout(()=>{
-        mainFunction();
-    },2000);
-}
-
+window.onload = function () {
+  console.log("Hi ");
+  setTimeout(() => {
+    mainFunction();
+  }, 2000);
+};
 
 function mainFunction() {
-    let div = document.getElementById('loading');
-    div.remove();
-   
-    let main_container = document.getElementById("main-container");
-    main_container.innerHTML = `<h2 class="heading">Counter</h2>
+  let div = document.getElementById("loading");
+  div.remove();
+
+  let main_container = document.getElementById("main-container");
+  main_container.innerHTML = `<h2 class="heading">Counter</h2>
     <button id = "decriment" class = "fade" >-</button>
     <p>${cnt}</p>
     <button id="increment" onclick = "incr()">+</button>`;
-
 }
 function incr() {
-    cnt = cnt + 1;
-    let main_container = document.getElementById("main-container");
-    main_container.innerHTML = `
+  cnt = cnt + 1;
+  let main_container = document.getElementById("main-container");
+  main_container.innerHTML = `
     <h2 class="heading">Counter</h2>
     <button id = "decriment" onclick = "decr()">-</button>
     <p>${cnt}</p>
     <button id="increment" onclick = "incr()">+</button>`;
 }
 function decr() {
-    cnt = cnt-1;
-    let main_container = document.getElementById("main-container");
-    if(cnt==0)
-    {
-        main_container.innerHTML = `
+  cnt = cnt - 1;
+  let main_container = document.getElementById("main-container");
+  if (cnt == 0) {
+    main_container.innerHTML = `
         <h2 class="heading">Counter</h2>
         <button id = "decriment" class="fade" >-</button>
         <p>${cnt}</p>
         <button id="increment" onclick = "incr()">+</button>`;
-    }
-    else{
-        main_container.innerHTML = `
+  } else {
+    main_container.innerHTML = `
         <h2 class="heading">Counter</h2>
         <button id = "decriment" onclick = "decr()">-</button>
         <p>${cnt}</p>
         <button id="increment" onclick = "incr()">+</button>`;
-    }
-  
+  }
 }
